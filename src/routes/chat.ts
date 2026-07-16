b2fb41ca20b93626403e18ce70aabe8c122dbace
hesantoshm <51165680+hesantoshm@users.noreply.github.com>
2026-07-16 09:24:23 +0530

    fixes

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,11 +1,10 @@
-import express from 'express';
-import { vehicleAgent } from '../agents/vehicleAgent';
+import express = require('express');
+const { vehicleAgent } = require('../agents/vehicleAgent');
 let router = express.Router();
-import {ApiService} from '../services/api.service';
-const apiService = new ApiService();
+
 router.post('/', async (req, res) => {
    const messages = req.body.messages;
    const response = await vehicleAgent(messages);
    res.json(response);
 })
-export default router;
+export = router;
